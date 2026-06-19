2a83bf6330f6badb068d15c45e44e4c33a05279b
theSirD <isaev-daniel17@yandex.ru>
2026-06-19 14:22:37 +0300

    chore: postgres multi-db init

---
+++ docker/postgres/init-multiple-databases.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE DATABASE catalog_db;
+    CREATE DATABASE inventory_db;
+    CREATE DATABASE payment_db;
+    CREATE DATABASE notification_db;
+EOSQL
